d9af11ab2d17275c9617d0af3fe76281e43aef88
Александр Иванов <saanyyaaa@gmail.com>
2021-01-12 13:49:51 +0300

    Стартовый шаблон и настройка webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const htmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        '@babel/polyfill',
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: __dirname + '/dist/'
+    },
+    plugins: [
+        new htmlPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+        ]
+    }
+}
